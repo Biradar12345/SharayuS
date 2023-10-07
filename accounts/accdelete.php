f5a9dd8400c0ccbf327de97badbe1c21e2eb70c0
Biradar12345 <47688824+Biradar12345@users.noreply.github.com>
2023-10-07 17:14:06 +0530

    accounts

---
+++ accounts/accdelete.php
@@ -0,0 +1,28 @@
+<?php
+// Variables
+
+$User = "root";
+$Password = "";
+$Database = "forum";
+$Host = "localhost";
+$sqlDate = date('Y-m-d H:i:s'); 
+
+		// Connect to the server
+	$con = 	mysqli_connect($Host, $User, $Password,$Database) or die (mysqli_error()); 
+		//Check connectivity
+		// Delete data into DB
+		
+		$del_rec = $_GET['del'];
+		
+		$query = "DELETE FROM user WHERE uid='$del_rec'";
+		
+		if(mysqli_query($con,$query)){
+			echo "<script>window.open('accountpanel.php?deleted=Record Deleted ','_self')</script>";
+		}
+		
+		
+		
+		
+		
+?>
+		
